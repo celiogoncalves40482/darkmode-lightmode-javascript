c39012239d070124341afdab518f875d993c964d
Celio Goncalves <celiogoncalves40482@gmail.com>
2022-05-13 10:34:37 -0300

    projeto javascript

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function changeMode() {
+    changeClasses();
+    changeText();
+}
+
+function changeClasses() {
+    button.classList.toggle('dark-mode');
+    h1.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+}
+
+function changeText() {
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if (button.classList.contains(dakrModeClass)) {
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + " ON";
+
+}
+
+const dakrModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('titulo-pagina');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode)
